38e7ee1ad735b6ebed8de30f7b34210e03fd2742
Calvin Wu <calvin.wu@addepar.com>
2023-08-27 22:00:34 -0400

    refactor

--- scripts/testng2junit.py
+++ scripts/testng2junit.py
@@ -24,7 +24,7 @@ import sys
 import re
 
 
-def migrateImports(content):
+def migrate_imports(content):
   """Updates import statements from TestNG to JUnit."""
   content_new = re.sub('org.testng.annotations.Test', 'org.junit.Test', content)
 
@@ -71,7 +71,7 @@ import com.google.inject.Injector;''', content_new)
   return content_new
 
 
-def migrateAnnotations(content):
+def migrate_testng_annotations(content):
   content_new = re.sub('@Test\npublic class', 'public class', content)
 
   content_new = re.sub('@BeforeMethod', '@Before', content_new)
@@ -79,7 +79,7 @@ def migrateAnnotations(content):
   return content_new
 
 
-def migrateDataProviders(content):
+def migrate_data_providers(content):
   """TestNG allows a DataProvider to be renamed."""
   # Make a list of tuples mapping the
   # new name to original name.
@@ -117,7 +117,7 @@ def migrateDataProviders(content):
   return content_new
 
 
-def migrateExceptions(content):
+def migrate_exceptions(content):
   content_new = re.sub('expectedExceptions', 'expected', content)
 
   exception_patt = re.compile(r'expected\s?=\s?{(.*)}')
@@ -126,7 +126,7 @@ def migrateExceptions(content):
   return content_new
 
 
-def migrateAsserts(content):
+def migrate_asserts(content):
   """Converts TestNG assertions to JUnit."""
   # TestNG has an overload for assertEquals that takes parameters:
   # obj1, obj2, message. JUnit also has this overload but takes parameters:
@@ -180,7 +180,7 @@ public class SomeTest {
   }
 }
 """
-def migrateGuiceAnnotation(content):
+def migrate_guice_annotation(content):
     if '@Guice' not in content:
         return content
 
@@ -270,12 +270,12 @@ def migrateTestFiles(test_dir):
         with open(file_name, 'r') as f:
             print("Converting ", file_name)
             content = f.read()
-            content_new = migrateImports(content)
-            content_new = migrateAnnotations(content_new)
-            content_new = migrateDataProviders(content_new)
-            content_new = migrateGuiceAnnotation(content_new)
-            content_new = migrateExceptions(content_new)
-            content_new = migrateAsserts(content_new)
+            content_new = migrate_imports(content)
+            content_new = migrate_testng_annotations(content_new)
+            content_new = migrate_data_providers(content_new)
+            content_new = migrate_guice_annotation(content_new)
+            content_new = migrate_exceptions(content_new)
+            content_new = migrate_asserts(content_new)
             with open(file_name, 'w') as fn:
                 fn.write(content_new)
 

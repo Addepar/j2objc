bd79edcdc761460950b9081ad79834c0d42b2ec5
Zhenfei Tai <zhenfei.tai@addepar.com>
2023-11-27 19:36:08 -0800

    upd

--- scripts/testng-junit/src/tests/setup.py
+++ scripts/testng-junit/src/tests/setup.py
@@ -2,8 +2,10 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+
 import testng2junit5
 import junit4_to_junit5
+
 
 ###
 # Strip content for equality comparison.

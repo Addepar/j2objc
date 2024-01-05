59c5dd25ac5db218e746ec4cf36b70276f5fb16c
Calvin Wu <calvin.wu@addepar.com>
2024-01-05 16:44:32 -0500

    update

--- scripts/testng-junit/src/junit4_to_junit5.py
+++ scripts/testng-junit/src/junit4_to_junit5.py
@@ -71,16 +71,21 @@ def migrate_annotations(content):
 
 
 def migrate_mockito_rule_annotation(content):
-    if 'MockitoRule' not in content:
+
+    if 'MockitoExtension' in content or '@Mock' not in content:
         return content
 
     content_new = re.sub(
-      'import org.mockito.junit.MockitoRule;',
-      'import org.junit.jupiter.api.extension.ExtendWith;\nimport org.mockito.junit.jupiter.MockitoExtension;', content)
+      'import org.mockito.Mock;',
+      '''import org.mockito.Mock;
+import org.junit.jupiter.api.extension.ExtendWith;      
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+''', content)
 
-    content_new = re.sub('public class', '@ExtendWith(MockitoExtension.class)\npublic class', content_new)
-    pattern = re.compile(r'^\s*@Rule\s+public\s+MockitoRule\s+rule\s*=\s*.*$', re.MULTILINE)
-    content_new = re.sub(pattern, '', content_new)
+    content_new = re.sub('public class', '@ExtendWith(MockitoExtension.class)\n'
+                                         '@MockitoSettings(strictness = Strictness.LENIENT)\npublic class', content_new)
     return content_new
 
 
@@ -186,12 +191,24 @@ def migrate_asserts(content):
     content_new = re.sub('com.addepar.infra.library.lang.assertion.Assertions.assertEquals',
                          'org.junit.jupiter.api.Assertions.assertEquals', content_new)
 
+    content_new = re.sub('com.addepar.infra.library.lang.assertion.Assert.assertEquals',
+                         'org.junit.jupiter.api.Assertions.assertEquals', content_new)
+
+    content_new = re.sub('com.addepar.infra.library.lang.assertion.Assert.assertTrue',
+                         'org.junit.jupiter.api.Assertions.assertTrue', content_new)
+
+    content_new = re.sub('com.addepar.infra.library.lang.assertion.Assert.assertFalse',
+                         'org.junit.jupiter.api.Assertions.assertFalse', content_new)
+
     content_new = re.sub('com.addepar.infra.library.lang.assertion.Assert;',
                          'org.junit.jupiter.api.Assertions;', content_new)
 
-    content_new = re.sub('    Assert.assert', '    Assertions.assert', content_new)
-    content_new = re.sub(r'Assert\.fail', 'Assertions.fail', content_new)
+    content_new = re.sub('org.testng.Assert',
+                         'org.junit.jupiter.api.Assertions', content_new)
 
+    content_new = re.sub('    Assert.assert', '    Assertions.assert', content_new)
+    content_new = re.sub('Assert.fail', 'Assertions.fail', content_new)
+    content_new = re.sub('Assert.assertEquals', 'Assertions.assertEquals', content_new)
 
     return content_new
 

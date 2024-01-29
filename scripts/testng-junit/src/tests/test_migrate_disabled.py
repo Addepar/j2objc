733122e5061457386a1860d20d79ee20af270cba
Chris MacDonald <chris.macdonald@addepar.com>
2024-01-29 13:19:45 -0800

    simplify

--- scripts/testng-junit/src/tests/test_migrate_disabled.py
+++ scripts/testng-junit/src/tests/test_migrate_disabled.py
@@ -3,13 +3,9 @@ from setup import testng2junit5
 def test_migrate_enabled_false():
     original = """
 @Test(enabled=false)
-@User(UserRole.ADMIN)
-public void secIdCreate() throws UtilsException {
 """
     expected = """
 @Disabled @Test
-@User(UserRole.ADMIN)
-public void secIdCreate() throws UtilsException {
 """
     actual = testng2junit5.migrate_testng_annotations(original)
     assert expected == actual
